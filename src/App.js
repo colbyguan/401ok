8501318dcd1412a42d555a3c12f968286bab83a5
colbyguan <colbyguan@gmail.com>
2019-02-03 14:03:53 -0800

    moar ui

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   numbers() {
     let monthlySalary = this.state.annualSalary / 12;
     let monthlyContrib = this.state.contrib / 12;
-    let contribPercent = this.state.contrib / this.state.annualSalary;
+    let contribPercent = this.state.contrib / this.state.annualSalary * 100;
     let earningsAfterTax = (100 - this.state.tax)/100 * (monthlySalary - monthlyContrib);
     let lossToTax = this.state.tax/100 * (monthlySalary - monthlyContrib);
     let earningsAfterAll = earningsAfterTax - this.state.expenses;
@@ -38,30 +38,53 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container">
-          <form>
-            <div className="form-group">
-              <label htmlFor="salary-annual">Annual salary</label>
-              <input type="number" className="form-control" id="salary-annual" placeholder="($)0.00" onChange={this.updateValue('annualSalary')} />
+        <p></p>
+          <div className="form-card">
+            <div className="form">
+              <div className="input-field">
+                <label htmlFor="salary-annual">($) Annual salary</label>
+                <input type="number" className="form-control" id="salary-annual" onChange={this.updateValue('annualSalary')} />
+                <small>Your pre-tax salary in your offer letter</small>
+              </div>
+              <div className="input-field">
+                <label htmlFor="contrib-annual">($) Annual 401k contribution (dollar amount)</label>
+                <input type="number" className="form-control" id="contrib-annual" onChange={this.updateValue('contrib')} />
+                <small>Maximum of $19,000 in 2019. See <a href="https://www.irs.gov/retirement-plans/plan-participant-employee/retirement-topics-401k-and-profit-sharing-plan-contribution-limits">here</a> for details</small>
+              </div>
+              <div className="input-field">
+                <label htmlFor="tax-rate">(%) Estimated tax rate</label>
+                <input type="number" className="form-control" id="contrib-annual" onChange={this.updateValue('tax')} />
+                <small>Out of 100%. See <a href="https://www.google.com/search?q=tax+bracket">here</a> for tax brackets</small>
+              </div>
+              <div className="input-field">
+                <label htmlFor="expenses">($) Total monthly expenses (eg. rent)</label>
+                <input type="number" className="form-control" id="expenses" onChange={this.updateValue('expenses')} />
+              </div>
             </div>
-            <div className="form-group">
-              <label htmlFor="contrib-annual">Annual 401k contribution (dollar amount)</label>
-              <input type="number" className="form-control" id="contrib-annual" placeholder="($)0.00" onChange={this.updateValue('contrib')} />
+            <div className="results">
+              <div className="row">
+                <div className="col s6 labels">
+                  <p>Desired contribution percentage</p>
+                  <p>Loss to taxes</p>
+                  <p>Monthly earnings after tax:</p>
+                  <p>Monthly earnings after tax and expenses:</p>
+                </div>
+                <div className="col s6 numbers">
+                  <p>{numbers[0]}%</p>
+                  <p>${numbers[1]}</p>
+                  <p>${numbers[2]}</p>
+                  <p>${numbers[3]}</p>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col s12">
+                  <small>By contributing more, you may lose less money to taxes with the tradeoff of having less monthly take-home earnings</small>
+                  <br /><br />
+                  <small><strong>Note: this app does not collect or send any of the entered or calculated data.</strong></small>
+                </div>
+              </div>
             </div>
-            <div className="form-group">
-              <label htmlFor="tax-rate">Estimated tax rate</label>
-              <input type="number" className="form-control" id="contrib-annual" placeholder="0(%)" onChange={this.updateValue('tax')} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="tax-rate">Total monthly expenses (eg. rent)</label>
-              <input type="number" className="form-control" id="expenses" placeholder="($)0.00" onChange={this.updateValue('expenses')} />
-            </div>
-          </form>
-          <ul className="list-group">
-            <li className="list-group-item">Contribution percentage: {numbers[0]}</li>
-            <li className="list-group-item">Money lost to taxes: {numbers[1]}</li>
-            <li className="list-group-item">Monthly earnings after tax: {numbers[2]}</li>
-            <li className="list-group-item">Monthly earnings after tax and expenses: {numbers[3]}</li>
-          </ul>
+          </div>
         </div>
       </div>
     );
