77861bf01cb8be7341e748092ca92c2f7ac4a16c
Colby Guan <6518677+colbyguan@users.noreply.github.com>
2021-10-24 22:12:30 -0700

    add fill-with-example button (#23)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const EXAMPLE = {
+  annualSalary: 56000,
+  contrib: 3920,
+  tax: 22,
+  expenses: 1100
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -11,6 +18,9 @@ class App extends Component {
       tax: 0,
       expenses: 0
     }
+  }
+  fillExample = () => {
+    this.setState(EXAMPLE);
   }
   updateValue = (key) => {
     return (event) => {
@@ -38,27 +48,30 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container">
-        <p></p>
+          <br /><br /><br />
           <div className="form-card">
             <div className="form">
+              <div className="example-btn-container">
+                <a className="example-btn waves-effect waves-light btn-small btn-flat green lighten-4" onClick={this.fillExample}>fill with example</a>
+              </div>
               <div className="input-field">
                 <label htmlFor="salary-annual">($) Annual salary</label>
-                <input type="number" className="form-control" id="salary-annual" onChange={this.updateValue('annualSalary')} />
                 <small>Your pre-tax salary in your offer letter</small>
+                <input type="number" className="form-control" id="salary-annual"  value={this.state.annualSalary} onChange={this.updateValue('annualSalary')} />
               </div>
               <div className="input-field">
                 <label htmlFor="contrib-annual">($) Annual 401k contribution (dollar amount)</label>
-                <input type="number" className="form-control" id="contrib-annual" onChange={this.updateValue('contrib')} />
                 <small>Maximum of $19,000 in 2019. See <a href="https://www.irs.gov/retirement-plans/plan-participant-employee/retirement-topics-401k-and-profit-sharing-plan-contribution-limits">here</a> for details</small>
+                <input type="number" className="form-control" id="contrib-annual"  value={this.state.contrib} onChange={this.updateValue('contrib')} />
               </div>
               <div className="input-field">
                 <label htmlFor="tax-rate">(%) Estimated tax rate</label>
-                <input type="number" className="form-control" id="contrib-annual" onChange={this.updateValue('tax')} />
                 <small>Out of 100%. See <a href="https://www.google.com/search?q=tax+bracket">here</a> for tax brackets</small>
+                <input type="number" className="form-control" id="contrib-annual"  value={this.state.tax} onChange={this.updateValue('tax')} />
               </div>
               <div className="input-field">
                 <label htmlFor="expenses">($) Total monthly expenses (eg. rent)</label>
-                <input type="number" className="form-control" id="expenses" onChange={this.updateValue('expenses')} />
+                <input type="number" className="form-control" id="expenses" value={this.state.expenses} onChange={this.updateValue('expenses')} />
               </div>
             </div>
             <div className="results">
